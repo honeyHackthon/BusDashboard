ea6026acecfdd787d602fec66f9f1c5a4ef1f5f9
dharmendraJavawale <dharmendra.kumar@ceitasl.com>
2020-08-29 11:23:47 +0530

    First commit

---
+++ public-transport-dashboard/src/main/java/com/honeywell/hackathon/publictransportdashboard/PublicTransportDashboardApplication.java
@@ -0,0 +1,13 @@
+package com.honeywell.hackathon.publictransportdashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PublicTransportDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PublicTransportDashboardApplication.class, args);
+	}
+
+}
---
+++ public-transport-dashboard/src/test/java/com/honeywell/hackathon/publictransportdashboard/PublicTransportDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.honeywell.hackathon.publictransportdashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PublicTransportDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
